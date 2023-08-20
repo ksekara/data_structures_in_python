424fb4d06eeac33a31d81e48df88593a54b88c81
ksekara <jsekara@gmail.com>
2023-08-20 17:18:29 +0530

    pop method added

--- linked_list.py
+++ linked_list.py
@@ -27,6 +27,22 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
 
 my_linked_list = LinkedList(4)
 my_linked_list.append(3)
